ecc581ae1e449a6ca41ec8599091bbd8c5e2b021
李梓铭 <962292139@qq.com>
2020-11-04 10:02:05 +0800

    feat(单元测试)： add karma 单元测试

--- src/packages/button.vue
+++ src/packages/button.vue
@@ -1,6 +1,6 @@
 <template>
-  <button class="poly-button">
-    <span v-if="this.$slots.default">
+  <button class="my-button" :class="btnClass" @click="$emit('click', $event)">
+    <span v-if="$slots.default">
       <slot></slot>
     </span>
   </button>
@@ -9,6 +9,30 @@
 <script>
 export default {
   name: 'my-button',
+  props: {
+    type: {
+      type: String,
+      default: '',
+      validator: function(type) {
+        let typeArr = ['primary', 'warning', 'danger', 'success', 'info']
+        if (type && typeArr.indexOf(type) == -1) {
+          console.error(
+            'type的类型必须为primary, warming, danger, success, info',
+          )
+        }
+        return true
+      },
+    },
+  },
+  computed: {
+    btnClass() {
+      let classes = []
+      if (this.type) {
+        classes.push(`my-button-${this.type}`)
+      }
+      return classes
+    },
+  },
 }
 </script>
 
@@ -20,7 +44,7 @@ $color: #606266;
 $border-color: #dcdfe6;
 $background: #ecf5ff;
 $active-color: #3a8ee6;
-.poly-button {
+.my-button {
   border-radius: $border-radius;
   border: 1px solid $border-color;
   color: $color;
@@ -43,6 +67,42 @@ $active-color: #3a8ee6;
     border-color: $active-color;
     background-color: $background;
     outline: none;
+  }
+  // 因为五中类型的按钮样式一个个写太过复杂，这里采用scss的循环遍历@each来写
+  // 格式：@each 类型,颜色  in (key:值) {
+  //  #{}取值表达式
+  // }
+
+  $color-list: (
+    // 定义颜色maps，类似于数组，但是里面可以放键值，数组只能单个值
+      primary: $primary,
+    success: $success,
+    info: $info,
+    warning: $warning,
+    danger: $danger
+  );
+  // 循环颜色maps，两个参数第一个为键第二个为值
+  @each $c123, $c2 in $color-list {
+    &-#{$c123} {
+      background: #{$c2};
+      border: 1px solid #{$c2};
+      color: #fff;
+    }
+  }
+
+  $color-hover-list: (
+    primary: $primary-hover,
+    success: $success-hover,
+    info: $info-hover,
+    warning: $warning-hover,
+    danger: $danger-hover,
+  );
+  @each $type, $color in $color-hover-list {
+    &-#{$type}:hover {
+      background: #{$color};
+      border: 1px solid #{$color};
+      color: #fff;
+    }
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
     <my-button />
     <my-button>默认按钮</my-button>
     <!--     // 带类型的按钮 -->
-    <my-button type="primary">主要按钮</my-button>
+    <my-button type="primary" @click="handleBtn">主要按钮</my-button>
     <my-button type="warning">警告按钮</my-button>
     <my-button type="danger">危险按钮</my-button>
     <my-button type="success">成功按钮</my-button>
@@ -19,5 +19,10 @@
 export default {
   name: 'Home',
   components: {},
+  methods: {
+    handleBtn() {
+      return 1
+    },
+  },
 }
 </script>
